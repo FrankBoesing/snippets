96cb8e42cab36f2a81733d5b1d0dab7c627f3e15
Frank <FrankBoesing@users.noreply.github.com>
2018-11-19 23:33:46 +0100

    Update optimized.h

--- optimized.h
+++ optimized.h
@@ -24,3 +24,27 @@ int16_t abs16(int16_t val)
     result = val;
   return result;
 }
+
+
+//Superfast strlen
+
+#define hasNulByte(x) ((x - 0x01010101) & ~x & 0x80808080)
+#define SW (sizeof (int) / sizeof (char))
+
+int xstrlen (const char *s) {
+const char *p;
+int d;
+
+    p = s - 1;
+    do {
+        p++;
+        if ((((int) p) & (SW - 1)) == 0) {
+            do {
+                d  = *((int *) p);
+                p += SW;
+            } while (!hasNulByte (d));
+            p -= SW;
+        }
+    } while (*p != 0);
+    return p - s;
+}
